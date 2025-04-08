e6fbaf40f183163b60f8c7bd35ae2c73b32f4437
RobbyFachrezaPutra <robbyfachrezaputra@gmail.com>
2025-04-08 21:04:18 +0700

    Read a Purchase Order by ID

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import { readData } from "./utils/read.data.json";
+import { error } from "console";
 
 const port = 8000;
 
@@ -9,20 +10,45 @@ const app: Application = express();
 app.use(express.json());
 
 app.get("/purchase-orders", (req: Request, res: Response) => {
-    try {
-        const data = readData();
-        const orderList = data.purchaseOrders;
+  try {
+    const data = readData();
+    const orderList = data.purchaseOrders;
 
-        res.status(200).json({
-          message: "Get purchase orders successfully",
-          data: orderList
-        });
-    } catch (err: any) {
-        res.status(400).json({
-          message: err.message,
-          data: {},
-        });
-    }
+    res.status(200).json({
+      message: "Get purchase orders successfully",
+      data: orderList,
+    });
+  } catch (err: any) {
+    res.status(400).json({
+      message: err.message,
+      data: {},
+    });
+  }
+});
+
+// Read a Purchase Order by ID
+
+app.get("/purchase-orders/:id", (req: Request, res: Response) => {
+  try {
+    const orderList = readData().purchaseOrders;
+
+    const { id } = req.params;
+    const purchaseOrder = orderList.find(
+      (order: any) => order.id === parseInt(id)
+    );
+
+    if (!purchaseOrder) throw new Error("Purchase order not found");
+
+    res.status(200).json({
+      message: "Get purchase order successfully",
+      data: purchaseOrder,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      message: err.message,
+      data: {},
+    });
+  }
 });
 
 // ERROR HANDLING MIDDLEWARE
